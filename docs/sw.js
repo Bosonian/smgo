db2b6aacdf94435cd715fb71249219dc3dbd5f38
Bosonian <bosdeepak@gmail.com>
2026-05-19 16:44:42 +0200

    Bump SW cache to v8 to force update

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smgo-v7';
+const CACHE = 'smgo-v8';
 const SHELL = ['./', './index.html', './app.js', './style.css', './manifest.json',
                './icons/icon-192.png', './icons/icon-512.png'];
 
